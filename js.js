92636e261ac15a1c84e3905b92a314c80ec5c6f1
aminriyad <aminriyad@hotmail.com>
2022-04-26 08:17:29 -0500

    main

--- js.js
+++ js.js
@@ -1,18 +1,28 @@
-let myleads = []
-myleadsarray = JSON.parse(myleads)
-myleadsarray.PUSH("AMIN")
-myleadsstring = JSON.stringify(myleadsarray)
-console.log( typeof myleadsstring)
-const inputEl = document.getElementById('input-el')
-const inputBtn = document.getElementById('input-btn')
-const ulEl= document.getElementById('ul-el')
+let myLeads = []
+const inputEl =document.getElementById ("input-el")
+const saveBtn = document.getElementById ("input-btn")
+const ulEl = document.getElementById ("ul-el")
+const deleteBtn = document.getElementById ("delete-btn")
+const leadsFromLocalStorage =JSON.parse( localStorage. getItem("myLeads") )
+const tabBtn = document.getElementById ("tab-btn")
 
-localStorage.setItem(amin, ahmed)
 
-console.log(localstorage)
-localStorage.clear()
+if (leadsFromLocalStorage) {
+myLeads = leadsFromLocalStorage
+render (myLeads)
+}
 
-inputBtn.addEventListener("click" , function() {
+
+tabBtn.addEventListener ("click", function (){
+chrome.tabs.query ({active: true, currentWindow: true}, function (tabs){
+myLeads.push(tabs[0].url)
+localStorage.setItem("myLeads", JSON.stringify (myLeads) )
+render (myLeads)
+    })
+})
+
+
+saveBtn.addEventListener("click" , function() {
     myleads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem(inputEl.value)
@@ -34,3 +44,17 @@ function renderleads() {
     }
     ulEl.innerHTML = listitems
 }
+
+
+deleteBtn.addEventListener("dblclick", function () {
+    localStorage.clear ()
+    myLeads = []
+    
+    render (myLeads)
+    })
+    saveBtn.addEventlistener ("click", function() {
+    myLeads.push (inputEl. value)
+    inputEl.value = ""
+    localStorage.setItem("my Leads", JSON.stringify (myLeads) )
+    render (myLeads)
+    })
